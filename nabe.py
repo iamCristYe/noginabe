bf8bcffb7c89a2585c04bbcd91faa640aa47a3d6
Your Name <you@example.com>
2025-12-02 14:45:35 +0800

    Test

--- nabe.py
+++ nabe.py
@@ -1,12 +1,13 @@
 import requests
 
-for i in range(129471 + 100, 129471 + 1000):
+for i in range(129884 + 100, 129884 + 1000):
     # https://video-tvtokyo.imgix.net/noginabe/20251030/image/20251030_noginabe_01_a_129070.jpg
     # https://video-tvtokyo.imgix.net/noginabe/20251102/image/20251102_noginabe_01_a_128759.jpg
     # https://video-tvtokyo.imgix.net/noginabe/20251109/image/20251109_noginabe_01_a_129012.jpg
     # https://video-tvtokyo.imgix.net/noginabe/20251116/image/20251116_noginabe_01_a_129202.jpg
     # https://video-tvtokyo.imgix.net/noginabe/20251123/image/20251123_noginabe_01_a_129471.jpg
-    url = f"https://video-tvtokyo.imgix.net/noginabe/20251130/image/20251130_noginabe_01_a_{i}.jpg"
+    # https://video-tvtokyo.imgix.net/noginabe/20251130/image/20251130_noginabe_01_a_129884.jpg
+    url = f"https://video-tvtokyo.imgix.net/noginabe/20251207/image/20251207_noginabe_01_a_{i}.jpg"
     print(url)
     # continue
     r = requests.get(url)
