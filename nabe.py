3b800770632fdcc39b4f18436b1b08caf9a6ceb7
Crist Ye <12642018+iamCristYe@users.noreply.github.com>
2025-12-12 20:30:51 +0800

    Update nabe.py

--- nabe.py
+++ nabe.py
@@ -8,7 +8,7 @@ for i in range(130158 + 100, 130158 + 1000):
     # https://video-tvtokyo.imgix.net/noginabe/20251123/image/20251123_noginabe_01_a_129471.jpg
     # https://video-tvtokyo.imgix.net/noginabe/20251130/image/20251130_noginabe_01_a_129884.jpg
     # https://video-tvtokyo.imgix.net/noginabe/20251207/image/20251207_noginabe_01_a_130158.jpg
-    url = f"https://video-tvtokyo.imgix.net/noginabe/20251215/image/20251215_noginabe_01_a_{i}.jpg"
+    url = f"https://video-tvtokyo.imgix.net/noginabe/20251214/image/20251214_noginabe_01_a_{i}.jpg"
     print(url)
     # continue
     r = requests.get(url)
