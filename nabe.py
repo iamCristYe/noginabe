4b8014b1d165f92ebdeed7e8266f6781ab35e0ae
Crist Ye <12642018+iamCristYe@users.noreply.github.com>
2025-12-30 09:45:23 +0000

    Update nabe.py

--- nabe.py
+++ nabe.py
@@ -1,6 +1,6 @@
 import requests
 
-for i in range(130456 + 200, 130458 + 1500):
+for i in range(131351 + 200, 131351 + 1500):
     # https://video-tvtokyo.imgix.net/noginabe/20251030/image/20251030_noginabe_01_a_129070.jpg
     # https://video-tvtokyo.imgix.net/noginabe/20251102/image/20251102_noginabe_01_a_128759.jpg
     # https://video-tvtokyo.imgix.net/noginabe/20251109/image/20251109_noginabe_01_a_129012.jpg
@@ -9,7 +9,8 @@ for i in range(130456 + 200, 130458 + 1500):
     # https://video-tvtokyo.imgix.net/noginabe/20251130/image/20251130_noginabe_01_a_129884.jpg
     # https://video-tvtokyo.imgix.net/noginabe/20251207/image/20251207_noginabe_01_a_130158.jpg
     # https://video-tvtokyo.imgix.net/noginabe/20251214/image/20251214_noginabe_01_a_130456.jpg
-    url = f"https://video-tvtokyo.imgix.net/noginabe/20260104/image/20260104_noginabe_01_a_{i}.jpg"
+    # https://video-tvtokyo.imgix.net/noginabe/20260104/image/20260104_noginabe_01_a_131351.jpg
+    url = f"https://video-tvtokyo.imgix.net/noginabe/20260111/image/20260111_noginabe_01_a_{i}.jpg"
     print(url)
     # continue
     r = requests.get(url)
